e899f3dc3c43b659fab5513aa6275c39193fa235
Himank Pathak <himank@peoplegrove.com>
2023-07-27 18:30:02 -0400

    [PG-31001]feat(Server): Update pageLoadTimeout

--- server.js
+++ server.js
@@ -6,17 +6,26 @@ const prerender = require('./lib');
 const prMemoryCache = require('prerender-memory-cache');
 
 const server = prerender({
-    chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--disable-dev-shm-usage'],
-    forwardHeaders: true,
-    chromeLocation: '/usr/bin/chromium-browser',
+  chromeFlags: [
+    '--no-sandbox',
+    '--headless',
+    '--disable-gpu',
+    '--remote-debugging-port=9222',
+    '--hide-scrollbars',
+    '--disable-dev-shm-usage',
+  ],
+  forwardHeaders: true,
+  chromeLocation: '/usr/bin/chromium-browser',
+  pageLoadTimeout: 30 * 1000,
 });
 
 const memCache = Number(process.env.MEMORY_CACHE) || 0;
 if (memCache === 1) {
-    server.use(prMemoryCache);
+  server.use(prMemoryCache);
 }
 
 // server.use(prerender.blacklist());
+// server.use(prerender.pgSiteNotFoundChecker());
 server.use(prerender.httpHeaders());
 server.use(prerender.removeScriptTags());
 
